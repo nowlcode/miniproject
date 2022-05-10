0c6077722d1646098d0be5fc390ee489d0408b22
monmonj0 <105182754+monmonj0@users.noreply.github.com>
2022-05-10 14:46:24 +0900

    Add files via upload
    게시글 등록, 수정 api

--- app.py
+++ app.py
@@ -38,5 +38,43 @@ def sign_up():
 
     return jsonify({'msg': '저장 완료!'})
 
+@app.route('/post/register', methods=['POST'])
+def post():
+    title_receive = request.form['title_give']
+    img_receive = request.form['image_give']
+    content_receive = request.form['content_give']
+    participants_receive = request.form['participants_give']
+    post_receive = request.form['postnum_give']
+
+    doc = {
+        'title':title_receive,
+        'image':img_receive,
+        'content':content_receive,
+        'participants': participants_receive,
+        'postnum': post_receive,
+    }
+    db.users.insert_one(doc)
+
+    return jsonify({'msg': '등록완료!'})
+
+@app.route('/post/edit', methods=['POST'])
+def edit():
+    title_receive = request.form['title_give']
+    img_receive = request.form['image_give']
+    content_receive = request.form['content_give']
+    participants_receive = request.form['participants_give']
+    post_receive = request.form['postnum_give']
+
+    doc = {
+        'title':title_receive,
+        'image':img_receive,
+        'content':content_receive,
+        'participants': participants_receive,
+        'postnum': post_receive,
+    }
+    db.users.insert_one(doc)
+
+    return jsonify({'msg': '수정 완료!'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
