9062e02654969f9ba0da006780d595e2878474cf
monmonj0 <105182754+monmonj0@users.noreply.github.com>
2022-05-10 15:46:02 +0900

    게시글 등록, 수정 api

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def edit():
     participants_receive = request.form['participants_give']
     post_receive = request.form['postnum_give']
 
-    db.posts.update_one({'postnum': post_receive}, {'$set':{'title' : title_receive}}, {'$set':{'image' : img_receive}}, {'$set':{'content' : content_receive}}, {{'$set':{'participants': participants_receive}}})
+    db.posts.update_one({'postnum': post_receive}, {'$set':{'title' : title_receive}}, {'$set':{'image' : img_receive}}, {'$set':{'content' : content_receive}}, {'$set':{'participants': participants_receive}})
 
     return jsonify({'msg': '수정 완료!'})
 
